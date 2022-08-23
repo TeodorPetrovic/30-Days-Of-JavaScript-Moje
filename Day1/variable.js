857e5902166cb220bade1940e9af36aec18bd45c
tpetrovic <teodor.z.petrovic@gmail.com>
2022-08-23 12:01:52 +0200

    test

--- Day1/variable.js
+++ Day1/variable.js
@@ -97,7 +97,58 @@ console.log(challenge.match(/a/g)) //regular expression
 let concat1 = "30 Days of"
 let concat2 = " JavaScript"
 console.log(concat1.concat(concat2))
+//25
+console.log(challenge.repeat(2))
 
+//// Exercise Level 2
+//1
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+//2
+console.log("\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\"")
+//3
+console.log(+'10' === 10)
+//4
+console.log(Math.round(parseFloat("9.8")) == 10)
+//5
+console.log("python".includes("on") && "jargon".includes("on"))
+//6
+console.log("I hope this course is not full of jargon.".includes("jargon"))
+//7
+console.log(Math.round(Math.random() * 100))
+//8
+console.log(Math.round(Math.random() * (100 - 50) + 50))
+/* function randomInRange(min, max) {  
+    return Math.floor(Math.random() * (max - min) + min); 
+}*/
+//9
+console.log(Math.round(Math.random() * 255))
+//10
+let string2 = "JavaScript"
+console.log(string2[Math.round(Math.random() * string2.length)])
+//11
+console.log(
+    " 1 1 1 1 1 \n 2 1 2 4 8 \n 3 1 3 9 27 \n 4 1 3 16 64 \n 5 1 5 25 125"
+)
+//12
+let text4 = "You cannot end a sentence with because because because is a conjunction"
+console.log(text4.indexOf("because because because"))
+console.log(text4.lastIndexOf("because"))
+console.log(text4.substring(0, 31) + text4.substring(47 + 8, text4.length))
+
+//Exercise Level3
+//1
+let text5 = "Love is the best thing in this world. Some found their love and some are still looking for their love."
+console.log(text5.match(/love/g).length)
+//2
+let text6 = "You cannot end a sentence with because because because is a conjunction"
+console.log(text6.match(/because/g).length)
+//3
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+console.log(sentence.replaceAll(/%|@|\$|&|#|;/g, ""))
+//4
+let text7 = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+let numbers = text7.match(/[0-9]+/g)
+console.log("Anual Sallary: " + ((+numbers[0] + +numbers[2]) * 12 + +numbers[1]) )
 
 
 
