04aa7a5e05ed7b4c5f4a321073c907f63cac3369
tpetrovic <teodor.z.petrovic@gmail.com>
2022-08-23 08:46:40 +0200

    test

---
+++ Day1/introduction.js
@@ -0,0 +1,18 @@
+console.log("Hello There.")
+console.log(typeof 'Asabeneh') // string
+console.log(typeof "lkasdjdjl") // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+//1
+//comments can make code readable
+
+//2
+//Welcome to 30DaysOfjavaScript
+
+//3
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
---
+++ Day1/variable.js
@@ -0,0 +1,51 @@
+/* let var1 = "var1"
+let var2 = true
+let var3
+let var4 = null
+console.log(typeof var1)
+console.log(typeof var2)
+console.log(typeof var3)
+console.log(typeof var4)
+let myAge =  21
+let yourAge = 45
+console.log("I am", myAge, "years old.")
+console.log("You are", yourAge, "years old.") */
+
+//DAY 2
+
+const PI = Math.PI
+console.log(PI)
+const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\
+I am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
+Node.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \
+In the end of 2019, I was thinking to expand my teaching and to reach \
+to global audience and I started a Python challenge from November 20 - December 19.\
+It was one of the most rewarding and inspiring experience.\
+Now, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \
+I hope you are enjoying too."
+
+console.log(paragraph)
+
+//Template literals
+
+console.log('The sum of 2 and 3 is 5')
+
+let a = 2
+let b = 3
+
+console.log(`The sum of ${a} and ${b} is ${a + b}`) // Nije navodnik nego ovaj ` iznad tab
+
+console.log(`${a} is grater than ${b}: ${ a > b}`)
+
+let string = "30 days od coding"
+console.log(string.split(" "))
+
+//Day 2 training
+
+let challenge = "30 Days of JavaScript"
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(2, 21))
+console
